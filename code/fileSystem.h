a998a2b2f95f06ada5cce40ae6b7e6feff13b5ec
Jacky <94925733+jzhangg917@users.noreply.github.com>
2023-08-22 19:17:42 -0400

    Update fileSystem.h

--- code/fileSystem.h
+++ code/fileSystem.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <ctime> // Include for time_t
 
 using namespace std;
 
@@ -21,10 +22,14 @@ public:
 
     bool isDirectoryNode() const;
 
+    // Define a method to set the last modified time
+    void setLastModifiedTime(time_t lastModified);
+
 private:
     string name;
     bool isDirectory;
     vector<FileSystemNode*> children;
+    time_t lastModifiedTime; // Store last modified time
 };
 
 class FileSystem {
@@ -45,6 +50,15 @@ public:
     void moveFile(const string& sourceName, const string& destinationName);
     void deleteNode(const string& name);
     vector<FileSystemNode*> search(const string& name);
+
+    // Define a method to set file metadata, including creation time and last modified time
+    void setFileMetadata(const string& name, time_t creationTime, time_t lastModifiedTime);
+
+    // Define a method to update file metadata, specifically the last modified time
+    void updateFileMetadata(const string& name, time_t lastModifiedTime);
+
+    // Define a method to get the current directory name
+    string getCurrentDirectoryName() const;
 
 private:
     void deleteFileSystem(FileSystemNode* node);
